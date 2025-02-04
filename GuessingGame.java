b62f385d010ef63100897efc8c24c96efb76a9e6
Hexsl <47760583+Hexsl@users.noreply.github.com>
2025-02-04 17:18:54 -0500

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.InputMismatchException;
 
-public class Main
+public class GuessingGame
 {
 	public static void main(String[] args) {
 	    // Initialization of scanner object for user input through a CLI.
